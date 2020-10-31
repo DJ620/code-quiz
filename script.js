3148e0497cf98ee6561d7a3e91d3c71bd1bc95ac
DJ Hersh <DanielJHersh@gmail.com>
2020-10-30 22:28:42 -0700

    created functions for correct and incorrect answers

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ var main = document.querySelector("main");
 var homepage = document.querySelector("#homepage");
 var timer = document.querySelector("#timer");
 var start = document.querySelector("#start");
+var outcome = document.querySelector("#outcome");
+var correct = document.querySelectorAll(".correct");
+var incorrect = document.querySelectorAll(".incorrect");
 var question1 = document.querySelector("#question1");
 var finish = document.querySelector("#finish");
 
-var sectionArray = [homepage, question1, , finish]
-var counter = 1;
+var sectionArray = [question1, finish];
+var sectionIndex = 0;
+var counter = 60;
+var score = 0;
 
 // Functions
 function hide() {
@@ -18,9 +23,32 @@ function reveal(element) {
     element.style.display="block";
 }
 
+function right() {
+    outcome.textContent = "Correct!";
+    score++;
+}
+
+function wrong() {
+    outcome.textContent = "Wrong";
+    counter -= 5;
+}
+
+function reset() {
+    outcome.textContent = " ";
+}
+
+function next() {
+    setTimeout(function() { 
+    reset();
+    hide();
+    sectionIndex++;
+    reveal(sectionArray[sectionIndex]);
+    }, 1000);
+}
+
 //EventListeners
 start.addEventListener("click", function() {
-    hide(homepage);
+    homepage.style.display="none";
     reveal(question1);
     timer.textContent = counter;
     var timerInterval = setInterval(function() {
@@ -30,10 +58,24 @@ start.addEventListener("click", function() {
             clearInterval(timerInterval);
             hide();
             reveal(finish);
+            reset();
         }
     }, 1000); 
 })
 
+for (var i=0; i<correct.length; i++) {
+    correct[i].addEventListener("click", function() {
+        right();
+        next();
+    })
+}
+
+for (var i=0; i<incorrect.length; i++) {
+    incorrect[i].addEventListener("click", function() {
+        wrong();
+        next();
+    })
+}
+
 // Javascript to run when page first loads
 hide();
-reveal(homepage);
