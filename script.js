772f6915a946e89cb7fe5364c72540fae5b4a51c
DJ Hersh <DanielJHersh@gmail.com>
2020-10-30 18:48:35 -0700

    wrote a function that will hide all sections when it is called

--- script.js
+++ script.js
@@ -3,33 +3,37 @@ var main = document.querySelector("main");
 var homepage = document.querySelector("#homepage");
 var timer = document.querySelector("#timer");
 var start = document.querySelector("#start");
-var finished = document.querySelector("#finish");
+var question1 = document.querySelector("#question1");
+var finish = document.querySelector("#finish");
 
-var counter = 60;
+var sectionArray = [homepage, question1, , finish]
+var counter = 1;
 
 // Functions
-function hide(element) {
-    element.style.display="none";
+function hide() {
+    sectionArray.forEach(element=>element.style.display="none");
 }
 
 function reveal(element) {
     element.style.display="block";
 }
 
-function finish() {
-    main.appendChild(finished);
-}
-
 //EventListeners
 start.addEventListener("click", function() {
     hide(homepage);
+    reveal(question1);
     timer.textContent = counter;
     var timerInterval = setInterval(function() {
         counter--;
         timer.textContent = counter;
         if (counter === 0) {
             clearInterval(timerInterval);
-            finish();
+            hide();
+            reveal(finish);
         }
     }, 1000); 
 })
+
+// Javascript to run when page first loads
+hide();
+reveal(homepage);
