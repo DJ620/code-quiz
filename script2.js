162fe1438149b57cbd8f55779d991853d8085bc2
DJ Hersh <DanielJHersh@gmail.com>
2020-10-31 23:05:32 -0700

    added more questions

--- script2.js
+++ script2.js
@@ -10,7 +10,6 @@ var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
 var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
-var optionButton = document.querySelectorAll(".option");
 var finish = document.querySelector("#finish");
 var finalScore = document.querySelector("#final-score");
 
@@ -42,9 +41,65 @@ var question4 = {
     answers: ["formal", "basic", "semantic", "pseudo"],
     correct: "semantic"
 }
+var question5 = {
+    question: "Inside what tag should external stylesheets be linked?",
+    answers: ["header", "footer", "body", "head"],
+    correct: "head"
+}
+var question6 = {
+    question: "Which is the correct way to set text to bold in CSS?",
+    answers: ["font-style: bold", "font-weight: bold", "font-type: bold", "font = bold"],
+    correct: "font-weight: bold"
+}
+
+var question7 = {
+    question: "Which of the following is a popular CSS framework directed at responsive, mobile-first front-end web development?",
+    answers: ["Bootclick", "SASS", "jQuery", "Bootstrap"];
+    correct: "Bootstrap"
+}
+var question8 = {
+    question: "",
+    answers: [];
+    correct: ""
+}
+var question9 = {
+    question: "",
+    answers: [];
+    correct: ""
+}
+var question10 = {
+    question: "",
+    answers: [];
+    correct: ""
+}
+var question11 = {
+    question: "",
+    answers: [];
+    correct: ""
+}
+var question12 = {
+    question: "",
+    answers: [];
+    correct: ""
+}
+var question13 = {
+    question: "",
+    answers: [];
+    correct: ""
+}
+var question14 = {
+    question: "",
+    answers: [];
+    correct: ""
+}
+var question15 = {
+    question: "",
+    answers: [];
+    correct: ""
+}
 
 // Arrays
-var allQuestions = [question1, question2, question3, question4];
+var allQuestions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11, question12, question13, question14, question15];
 var options = [option1, option2, option3, option4];
 
 // Functions
@@ -65,7 +120,7 @@ function correct() {
 function incorrect() {
     outcome.textContent = "Wrong.";
     reveal(outcome);
-    counter -= 5;
+    counter -= 10;
 };
 
 function setOptions() {
