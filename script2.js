d37cbb0a48d27fbaa698907c088cf4ece4ee154f
DJ Hersh <DanielJHersh@gmail.com>
2020-10-31 20:32:04 -0700

    Trying out formatting each question as an object in javascript, input the first question

--- script2.js
+++ script2.js
@@ -10,19 +10,42 @@ var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
 var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
+var finish = document.querySelector("#finish");
+var finalScore = document.querySelector("#final-score");
 
-var counter = 60;
+var counter = 5;
+var score = 0;
+
+// Functions
+function hide(element) {
+    element.style.display = "none";
+}
+
+function reveal(element) {
+    element.style.display = "block";
+}
 
 //EventListeners
 start.addEventListener("click", function() {
-    homepage.style.display="none";
-    questionBlock.style.display = "block";
+    hide(homepage);
+    reveal(questionBlock);
     timer.textContent = counter;
     var timerInterval = setInterval(function() {
         counter--;
         timer.textContent = counter;
         if (counter === 0) {
             clearInterval(timerInterval);
+            hide(questionBlock);
+            reveal(finish);
+            finalScore.textContent = score;
         }
     }, 1000); 
 })
+
+// Questions
+var question1 = {
+    question: "Which of the following is not one of the three fundamental programming languages of the modern web?",
+    options: ["HTML", "CSS", "JavaScript", "Python"],
+    correct: "Python",
+    incorrect: ["HTML", "CSS", "JavaScript"]
+}
