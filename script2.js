3bfa27d6da4fec1a8c98dfc40feec8dacf51d8de
DJ Hersh <DanielJHersh@gmail.com>
2020-10-31 21:54:31 -0700

    Fixed some styling issues and working on a code that tells if the answer clicked is correct or incorrect

--- script2.js
+++ script2.js
@@ -15,7 +15,7 @@ var finish = document.querySelector("#finish");
 var finalScore = document.querySelector("#final-score");
 
 // Variables
-var counter = 5;
+var counter = 20;
 var questionCount = 1;
 var score = 0;
 
@@ -24,45 +24,49 @@ var question1 = {
     question: "Which of the following is not one of the three fundamental programming languages of the modern web?",
     answers: ["HTML", "CSS", "Bootstrap", "JavaScript"],
     correct: "Bootstrap"
+};
+var question2 = {
+    question: "What is HTML used for?",
+    answers: ["Styling content", "Writing content", "Handling logic", "Interacting with the user"],
+    correct: "Writing content"
 }
 
 // Arrays
-var allQuestions = [question1];
-var options = [option1.textContent, option2.innerHTML, option3.textContent, option4.textContent];
+var allQuestions = [question1, question2];
+var options = [option1, option2, option3, option4];
 
 // Functions
 function hide(element) {
     element.style.display = "none";
-}
+};
 
 function reveal(element) {
     element.style.display = "block";
-}
+};
 
 function correct() {
     outcome.textContent = "Correct!";
     reveal(outcome);
-}
+};
 
 function incorrect() {
     outcome.textContent = "Wrong.";
     reveal(outcome);
-}
+};
 
 function setOptions() {
     for (var i = 0; i < options.length; i++) {
-        options[i] = allQuestions[questionCount - 1].answers[i];
-    }
-}
+        options[i].textContent = allQuestions[questionCount - 1].answers[i];
+    };
+};
 
 function nextQuestion() {
     hide(outcome);
     questionNum.textContent = questionCount;
     question.textContent = allQuestions[questionCount - 1].question;
     setOptions();
-    console.log(options);
     questionCount++;
-}
+};
 
 //EventListeners
 start.addEventListener("click", function() {
@@ -81,5 +85,22 @@ start.addEventListener("click", function() {
             finalScore.textContent = score;
         }
     }, 1000); 
-})
+});
+
+for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", function() {
+        setTimeout(function() {
+            if (options[i] === allQuestions[questionCount - 1].correct) {
+                correct();
+                score++;
+            } else {
+                incorrect();
+                counter -= 5;
+            }
+        }, 500);
+        nextQuestion();
+    })
+}
+
+
 
