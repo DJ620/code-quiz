7819a348afb226b156505b84dd2484d2e9168dcf
DJ Hersh <DanielJHersh@gmail.com>
2020-11-02 22:59:05 -0800

    added code that populates the highscore board with previous scores, even if the page has been refreshed

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var restart = document.querySelector("#restart");
 // Variables-----------------------------------------------------------------
 
 // The variable used as my countdown/timer
-var counter = 60;
+var counter = 10;
 
 // The variable used to display what question number the user is on, and also used to determine which question to display on screen
 var questionCount = 1;
@@ -114,6 +114,13 @@ var question15 = {
 var allQuestions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11, question12, question13, question14, question15];
 var options = [option1, option2, option3, option4];
 var playersArr = [];
+var checkScores = JSON.parse(localStorage.getItem("players"));
+if (checkScores) {
+    for (var i =0; i<checkScores.length; i++) {
+        playersArr.push(checkScores[i]);
+    }
+    localStorage.setItem("players", JSON.stringify(playersArr));
+}
 
 // Functions-----------------------------------------------------------------
 
